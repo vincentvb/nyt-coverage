3b57064a534d411bd6b00c10af233b09c2daf422
Vincent Van Buskirk <vvanbuskirk805@gmail.com>
2022-01-12 17:56:47 -0500

    fix port

--- map/server.js
+++ map/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+const port = process.env.PORT || 3000
 
 app.get('/', (_, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
@@ -8,6 +9,6 @@ app.get('/', (_, res) => {
 
 app.use(express.static('public'))
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Listening at http://localhost:${port}`)
+app.listen(port, () => {
+    console.log(`Listening at port ${port}`)
 })
